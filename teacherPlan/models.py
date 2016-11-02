4a0744958915e7e7d5da3ae4df5205eea2fe4dce
w3prog <w3prog@gmail.com>
2016-11-03 02:39:44 +0300

    Добавлены тесты на проверку наличия страниц

---
+++ teacherPlan/models.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Create your models here.
--- teacherPlan/tests.py
+++ teacherPlan/tests.py
@@ -1,3 +1,35 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+c = Client()
+
+
+class GetPagesTest(TestCase):
+  """
+    Тесты на получение страниц
+  """
+  def test_index(self):
+    response = c.get('/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_planlist(self):
+    response = c.get('/listOfPlans')
+    self.assertEqual(response.status_code, 200)
+
+  def test_newPlan(self):
+    response = c.get('/makeNewPlan')
+    self.assertEqual(response.status_code, 200)
+
+  def test_plan(self):
+    response = c.get('/plan')
+    self.assertEqual(response.status_code, 200)
+
+  def test_simpleReport(self):
+    response = c.get('/managerReport')
+    self.assertEqual(response.status_code, 200)

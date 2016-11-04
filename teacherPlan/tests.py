5cf0d10a25ff5bead2e03fa4adc3cbb44d99adae
w3prog <w3prog@gmail.com>
2016-11-04 11:12:44 +0300

    исправление в тестах

--- teacherPlan/tests.py
+++ teacherPlan/tests.py
@@ -16,20 +16,20 @@ class GetPagesTest(TestCase):
   """
   def test_index(self):
     response = c.get('/')
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)
 
   def test_planlist(self):
     response = c.get('/listOfPlans')
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)
 
   def test_newPlan(self):
     response = c.get('/makeNewPlan')
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)
 
   def test_plan(self):
     response = c.get('/plan')
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)
 
   def test_simpleReport(self):
     response = c.get('/managerReport')
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)

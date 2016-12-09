d0a9229c34d1e2d9f0ef4374acba998bf9a54828
w3prog <w3prog@gmail.com>
2016-12-09 07:53:56 +0300

    Убрал лишний класс

--- teacherPlan/models.py
+++ teacherPlan/models.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from djangotoolbox.fields import EmbeddedModelField, ListField
 
-from moevmCommon.models.userProfile import UserProfile
 
-class teacher_plan(models.Model):
-  teacher = models.ForeignKey(UserProfile)
-  date = models.DateField(null=True)
-
-  research = ListField(EmbeddedModelField('Publication'))
-  disciplines = ListField(EmbeddedModelField('Comment'))
-  Participations = ListField(EmbeddedModelField('Comment'))
-  nirs = ListField(EmbeddedModelField('Comment'))
-
-  @property
-  def teacher_name(self):
-    return self.teacher.first_name + " " + self.teacher.last_name
-
-  def __str__(self):
-    return "Учебный план на " + self.date + " " + self.teacher_name
-
+@python_2_unicode_compatible
 class Qualification(models.Model):
   ql_date = models.DateTimeField(verbose_name="Период")
   for_ql = models.CharField(max_length=200, verbose_name="Форма повышения квалификации")
   doc = models.CharField(max_length=200, verbose_name="Документ")
 
+@python_2_unicode_compatible
 class AnotherWork(models.Model):
   work_date = models.DateTimeField(verbose_name="Период")
   v_work = models.CharField(max_length=200, verbose_name="Вид работы")
 
+@python_2_unicode_compatible
 class Remark(models.Model):
   rem_date = models.DateTimeField(verbose_name="Дата")
   rem = models.CharField(max_length=200, verbose_name="Характер замечания")
   position = models.CharField(max_length=200, verbose_name="Должность лица, вносящего замечание")
-  # sign = models.CharField(max_length=200, verbose_name="Подпись лица, вносящего замечание")
-  # sign_t = models.CharField(max_length=200, verbose_name="Подпись преподавателя")

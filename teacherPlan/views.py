47b9a8092e118cd076f4119e0bae2cdcfce6ad3e
w3prog <w3prog@gmail.com>
2016-12-21 02:40:49 +0300

    Исправление бага в планах

--- teacherPlan/views.py
+++ teacherPlan/views.py
@@ -91,13 +91,9 @@ def currentPlan(request):
     user_profile = UserProfile.get_profile_by_user(request.user)
     plan=None
     if month < 8:
-        plan = TeacherPlan.objects.exclude(person_profile = user_profile, start_year = (year - 1))
-        if not plan:
-            plan = TeacherPlan.objects.create(person_profile = user_profile, start_year = (year - 1))
+        plan = TeacherPlan.objects.get_or_create(person_profile = user_profile, start_year = (year - 1))
     else :
-        plan = TeacherPlan.objects.exclude(person_profile = user_profile, start_year = year)
-        if not plan :
-            plan = TeacherPlan.objects.create(person_profile=user_profile, start_year=(year))
+        plan = TeacherPlan.objects.get_or_create(person_profile = user_profile, start_year = year)
     return render(request, 'teacherPlan/plan.html',{'plan':plan,'user_profile':user_profile})
 
 @login_teacher_required(login_url="/teacherPlan/login")
